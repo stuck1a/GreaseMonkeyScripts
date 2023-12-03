3da9be83d62b5745237b0cec534a58b4c11edfb2
stuck1a <stuck1a@stuck1a.de>
2023-12-03 14:47:46 +0100

    [1.4] added updateURL to script meta

--- src/NuoFlix/EnhancedNuoFlix/Global/userscript_definition.js
+++ src/NuoFlix/EnhancedNuoFlix/Global/userscript_definition.js
@@ -2,7 +2,7 @@
 // @name            Enhanced NuoFlix
 // @name:de         NuoFlix 2.0
 // @namespace       http://tampermonkey.net/
-// @version         1.3.1
+// @version         1.4
 // @description     Adds a lot of improvements and new features to NuoFlix. See preview video for more details: https://www.youtube.com/watch?v=kxp2j46IWZA
 // @description:de  Fügt zahlreiche Verbesserungen und neue Funktionen zu NuoFlix hinzu. Weitere Details finden Sie im Vorschauvideo: https://www.youtube.com/watch?v=kxp2j46IWZA
 // @icon            https://nuoflix.de/favicon-16x16.png
@@ -15,5 +15,7 @@
 // @website         https://stuck1a.de/
 // @match           http*://nuoflix.de/*
 // @run-at          document-end
+// @updateURL       https://raw.githubusercontent.com/stuck1a/GreaseMonkeyScripts/main/dist/NuoFlix/EnhancedNuoFlix.user.js
+// @downloadURL     https://raw.githubusercontent.com/stuck1a/GreaseMonkeyScripts/main/dist/NuoFlix/EnhancedNuoFlix.user.js
 // @supportURL      mailto:dev@stuck1a.de?subject=Meldung zum Skript 'Enhanced NuoFlix'&body=Problembeschreibung, Frage oder Feedback:
 // ==/UserScript==
